83738fee61561bedab151f40d77182553c171c39
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2023-08-06 21:49:35 -0400

    run scripts

--- dev-mainnet.sh
+++ dev-mainnet.sh
@@ -1,8 +1,7 @@
 export RPC_USER='rpcuser'
 export RPC_PASS='rpcpass'
-export RPC_HOST='http://localhost:18334'
-export RPC_LIMIT=5
-#export RPC_HOST='http://144.76.223.222:3031/'
+export RPC_HOST='http://localhost:8334'
+export RPC_LIMIT=100
 export API_HOST='localhost:8080'
 export BLOCKS_PARSING_DEPTH=10
 go run main.go
---
+++ dev-testnet.sh
@@ -0,0 +1,7 @@
+export RPC_USER='rpcuser'
+export RPC_PASS='rpcpass'
+export RPC_HOST='http://localhost:18334'
+export RPC_LIMIT=5
+export API_HOST='localhost:8080'
+export BLOCKS_PARSING_DEPTH=10
+go run main.go
