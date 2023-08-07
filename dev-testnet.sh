a72fe91f6124f2b5f28d3866c75fa39e92bcadde
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2023-08-07 11:44:32 -0400

    dev run script udpate

--- dev-testnet.sh
+++ dev-testnet.sh
@@ -2,6 +2,7 @@ export RPC_USER='rpcuser'
 export RPC_PASS='rpcpass'
 export RPC_HOST='http://localhost:18334'
 export RPC_LIMIT=5
+#export RPC_HOST='http://144.76.223.222:3031/'
 export API_HOST='localhost:8080'
-export BLOCKS_PARSING_DEPTH=10
+export BLOCKS_PARSING_DEPTH=3
 go run main.go
